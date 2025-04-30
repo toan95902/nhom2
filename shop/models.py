5b6776282636f1bd30e46f6ca3dccf6a336fd266
toan95902 <toan95902@gmail.com>
2025-04-30 22:11:35 +0700

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -71,4 +71,11 @@ class Review(models.Model):
     def __str__(self):
         return f"Review for {self.product.name} by {self.user.username}"
     
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Giá tại thời điểm mua
 
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
