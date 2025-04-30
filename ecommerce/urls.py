7f39c081179f55b9d0fa8c5c7a33c8d599e63cc9
toan95902 <toan95902@gmail.com>
2025-04-30 22:07:12 +0700

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,8 +5,4 @@ from shop import api_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('api/products', api_views.product_list_api),
-    path('api/orders', api_views.order_list_api),
-    path("api/", include("shop.urls")),
-    path('grappelli/', include('grappelli.urls')),
 ]
